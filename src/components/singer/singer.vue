d0d1e5ccd2b21f81e79ce54c03cafa7a74d29414
Ccc1l <610178666@qq.com>
2018-03-22 23:42:43 +0800

    :sparkles::sparkles::sparkles: singerlist接口获取数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>
-      歌手页面
+  <div class="singer">
   </div>
 </template>
-<script>
+<script type="text/ecmascript-6">
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
 export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
