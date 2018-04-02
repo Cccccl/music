fe50c4947e8b77dc6126d9f0888fb8c32f5278b9
Ccc1l <610178666@qq.com>
2018-04-03 00:50:44 +0800

    :sparkles::sparkles::sparkles: 歌手详情

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li v-for="song in songs" v-bind:key="song.id" class="item">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    getDesc: function (song) {
+      return `${song.singer}.${song.ablum}`
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+@import "~common/less/variable";
+@import "~common/less/mixin";
+
+.song-list{
+    .item{
+        display: flex;
+        align-items: center;
+        box-sizing: border-box;
+        height: 64px;
+        font-size: @font-size-medium;
+        .content{
+            flex:1;
+            line-height: 20px;
+            overflow: hidden;
+            .name{
+                .no-wrap();
+                color: @color-text;
+            }
+            .desc{
+                .no-wrap();
+                margin-top: 4px;
+                color: @color-text-d;
+            }
+        }
+    }
+}
+</style>
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,12 +4,27 @@
       <i class="icon-back" @click="back"></i>
     </div>
     <h1 class="title">{{title}}</h1>
-    <div class="bg-image" :style="bgStyle">
-      <div class="filter"></div>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="filter" ref="filter"></div>
     </div>
+    <div class="bg-layer" ref="layer"></div>
+    <scroll
+      @scroll = "scroll"
+      :probe-type = "probeType"
+      :listen-scroll = "listenScroll"
+      :data="songs" class="list" ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 <script type="text/ecmascript-6">
+import SongList from 'base/song-list/song-list'
+import Scroll from 'base/scroll/scroll'
+
+const RESERVED_HEIGHT = 40
+
 export default {
   props: {
     bgImage: {
@@ -25,9 +40,60 @@ export default {
       default: () => []
     }
   },
+  data: function () {
+    return {
+      scrollY: 0
+    }
+  },
   methods: {
     back: function () {
       this.$router.go(-1)
+    },
+    scroll: function(pos) {
+      this.scrollY = pos.y
+    }
+  },
+  components: {
+    SongList,
+    Scroll
+  },
+  created: function () {
+    this.probeType = 3
+    this.listenScroll = true
+  },
+  mounted: function () {
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
+  },
+  watch: {
+    scrollY (newY) {
+      let translateY = Math.max(this.minTranslateY, newY)
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+      const percent = Math.abs(newY / this.imageHeight)
+      this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
+      this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      } else {
+        blur = Math.min(20 * percent, 20)
+      }
+      this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
+      this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style['transform'] = `scale(${scale})`
+      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
     }
   },
   computed: {
@@ -89,6 +155,22 @@ export default {
             height: 100%;
             background: rgba(7, 17, 27, 0.4)
         }
+    }
+    .bg-layer{
+      position: relative;
+      height: 100%;
+      background: @color-background;
+    }
+    .list{
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      width: 100%;
+      // overflow: hidden;
+      background: @color-background;
+      .song-list-wrapper{
+        padding: 20px 30px;
+      }
     }
 }
 </style>
