e1fa4fc96e4402f8ba2885b17469016d78846305
Ccc1l <610178666@qq.com>
2018-04-15 18:36:38 +0800

    :sparkles: js中的autopre的封装

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,6 +5,12 @@
     </div>
     <h1 class="title">{{title}}</h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper" ref="playBtn">
+        <div class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -16,14 +22,21 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 <script type="text/ecmascript-6">
 import SongList from 'base/song-list/song-list'
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import {prefixStyle} from 'common/js/dom'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   props: {
@@ -55,7 +68,8 @@ export default {
   },
   components: {
     SongList,
-    Scroll
+    Scroll,
+    Loading
   },
   created: function () {
     this.probeType = 3
@@ -73,27 +87,26 @@ export default {
       let scale = 1
       let blur = 0
       const percent = Math.abs(newY / this.imageHeight)
-      this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+      this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
       if (newY > 0) {
         scale = 1 + percent
         zIndex = 10
       } else {
         blur = Math.min(20 * percent, 20)
       }
-      this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
-      this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
       if (newY < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
       this.$refs.bgImage.style.zIndex = zIndex
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
-      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
     }
   },
   computed: {
@@ -145,6 +158,34 @@ export default {
         padding-top: 70%;
         transform-origin: top;
         background-size: cover;
+        .play-wrapper{
+          position: absolute;
+          bottom: 20px;
+          z-index: 50;
+          width: 100%;
+          .play{
+            box-sizing: border-box;
+            width: 135px;
+            padding: 7px 0;
+            margin: 0 auto;
+            text-align: center;
+            border: 1px solid @color-theme;
+            color: @color-theme;
+            border-radius: 100px;
+            font-size: 0;
+            .icon-play{
+              display: inline-block;
+              vertical-align: middle;
+              margin-right: 6px;
+              font-size: @font-size-medium-x;
+            }
+            .text{
+              display: inline-block;
+              vertical-align: middle;
+              font-size: @font-size-small;
+            }
+          }
+        }
         .filter{
             position: absolute;
             top: 0;
@@ -170,6 +211,12 @@ export default {
       background: @color-background;
       .song-list-wrapper{
         padding: 20px 30px;
+        .loading-container{
+          position: absolute;
+          width: 100%;
+          top: 50%;
+          transform: translateY(-50%);
+        }
       }
     }
 }
